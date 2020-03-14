5edf7d70c9e4d2571d5e414c82f0aaecbe9c9837
hhulsman <hhulsman@gmail.com>
2020-03-14 12:35:41 +0100

    New scripts 5_StartLab and 6_StopLab

---
+++ HHH/5_Start Lab.ps1
@@ -0,0 +1,47 @@
+﻿#requires -Modules Hyper-V
+
+
+# Script para arrancar un laboratorio
+
+# 1. Crear el switch virtual
+# 2. Conectar las NICs con nombre 'Management*' al switch
+# 3. Arrancar las VMs
+
+# Ejecutar en el Host Hyper-V ó en cliente W10
+
+
+# Importar modulo Hyper-V con prefijo, para distinguir comandos con el mismo nombre de VMware
+Import-Module -Prefix HV -Name Hyper-V
+
+
+# Valores
+$VMHost = 'Animp10'
+$AdapterPrefix = 'Management*'
+$VMSwitchName = 'WSLab3-Switch'
+$VMNames = 'WSLab3-DC', 'WSLab3-S2D1', 'WSLab3-S2D2', 'WSLab3-S2D3', 'WSLab3-S2D4', 'WSLab3-Win10-1', 'WSLab3-Win10-2', 'WSLab3-Win10-3', 'WSLab3-Win10-4'
+
+
+# 1. Crear el switch, si no existe
+if (!(Get-VMSwitch -Name $VMSwitchName -CimSession $VMHost -ErrorAction SilentlyContinue)) {
+    New-VMSwitch -SwitchType Private -Name $VMSwitchName -CimSession $VMHost
+}
+
+
+# 2. Conectar las NICs con nombre 'Management*' al switch
+foreach ($VMName in $VMnames) {
+
+    # Obtener todas las NICs de la VM cuyo nombre empiece por 'Management'
+    $NetAdapters = Get-VMNetworkAdapter -Name $AdapterPrefix -VMName $VMName -CimSession $VMHost
+
+    # Conectar las NICs de la VM al switch
+    Connect-VMNetworkAdapter -Name $NetAdapters.Name -VMName $VMName -SwitchName $VMSwitch.Name -CimSession $VMHost
+
+}
+
+# 3. Arrancar las VMs
+Start-HVVM -Name $VMNames -CimSession $VMHost
+
+break
+
+    # Eliminar el switch, al final del ejercicio
+    Remove-VMSwitch -Name $VMSwitchName -CimSession $VMHost -Force -Confirm:$false
---
+++ HHH/6_Stop Lab.ps1
@@ -0,0 +1,32 @@
+﻿#requires -Modules Hyper-V
+
+
+# Script para parar un laboratorio después de realizar pruebas. No es lo mismo que eliminar el lab, que se haría con Cleanup.ps1
+
+# 1. Apagar las VMs
+# 2. Eliminar el switch virtual (Las VM's se quedan sin switch configurado, pero se vuelven a conectar con '5_Start Lab.ps1'
+
+
+# Ejecutar en el Host Hyper-V ó en cliente W10
+
+
+# Importar modulo Hyper-V con prefijo, para distinguir comandos con el mismo nombre de VMware
+Import-Module -Prefix HV -Name Hyper-V
+
+
+# Valores
+$VMHost = 'Animp10'
+$AdapterPrefix = 'Management*'
+$VMSwitchName = 'WSLab3-Switch'
+$VMNames = 'WSLab3-DC', 'WSLab3-S2D1', 'WSLab3-S2D2', 'WSLab3-S2D3', 'WSLab3-S2D4', 'WSLab3-Win10-1', 'WSLab3-Win10-2', 'WSLab3-Win10-3', 'WSLab3-Win10-4'
+
+
+# 1. Apagar las VMs
+Stop-HVVM -Name $VMNames -CimSession $VMHost
+
+
+# 2. Eliminar el switch (si existe)
+if (Get-VMSwitch -Name $VMSwitchName -CimSession $VMHost -ErrorAction SilentlyContinue) {
+    Remove-VMSwitch -Name $VMSwitchName -CimSession $VMHost -Force -Confirm:$false
+}
+
