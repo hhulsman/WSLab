f07484ba4ab94e99fc4d12a68ca15f78b94ca245
hhulsman <hhulsman@gmail.com>
2020-08-04 09:29:51 +0200

    Undo change in CreateUnattendfileVHD

--- Scripts/2_CreateParentDisks.ps1
+++ Scripts/2_CreateParentDisks.ps1
@@ -56,10 +56,10 @@ If (-not $isAdmin) {
  <settings pass="specialize">
     <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
         <ComputerName>$Computername</ComputerName>
-<!--    <OEMInformation>
+        <OEMInformation>
           <SupportProvider>WSLab</SupportProvider>
           <SupportURL>https://aka.ms/wslab</SupportURL>
-        </OEMInformation>      *** HHH: Deleted, gives an error in W2016 DC *** -->
+        </OEMInformation>
         <RegisteredOwner>PFE</RegisteredOwner>
         <RegisteredOrganization>Contoso</RegisteredOrganization>
     </component>
